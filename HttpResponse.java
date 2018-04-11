cba9fb4e52083e699b2a194181fee52e7cf7afd6
Philam18 <philiplam818@gmail.com>
2018-04-10 21:06:42 -0400

    update

--- HttpResponse.java
+++ HttpResponse.java
@@ -15,7 +15,6 @@ public class HttpResponse {
     }else{
       f = new File(root + req.filename);
     }
-
     try {
       FileInputStream fis = new FileInputStream(f);
       response += "HTTP/1.1 200 \r\n";
@@ -30,7 +29,6 @@ public class HttpResponse {
       fis.close();
       String responseHeader = "";
     }catch(FileNotFoundException e){
-      //response = response.replace("200", "404");
       System.out.println("COULDNT FIND FILE: " + req.filename);
       try{
         File file404 = new File(root + "/404.html");
@@ -52,7 +50,6 @@ public class HttpResponse {
         //Ignore
       }
     }catch(Exception e){
-      //response = response.replace("200", "500");
       System.out.println("Something went wrong: " + e);
       try{
         File file500 = new File(root + "/500.html");
@@ -77,47 +74,3 @@ public class HttpResponse {
   }
 
 }
-
-/**
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-
-public class HttpResponse {
-
-  HttpRequest req;
-  String response;
-  String root = "~/Desktop/cps706/sample/";
-
-  public HttpResponse(HttpRequest request){
-    req = request;
-    File f = new File(root + req.filename);
-    try {
-
-      FileInputStream fis = new FileInputStream(f);
-
-      response += "HTTP/1.1 200 \r\n";
-      response += "Server: Our Java Server/1.0 \r\n";
-      response += "Connection: close \r\n";
-      response += "Content-Length: " + f.length() + "\r\n";
-      response += "\r\n";
-
-      int s;
-
-      while ((s = fis.read()) != -1 ) {
-        response += (char) s;
-      }
-      fis.close();
-
-      String responseHeader = "";
-    }catch(FileNotFoundException e){
-      //response = response.replace("200", "404");
-      reponse += "HTTP/1.1 404 \r\n";
-
-    }catch(Exception e){
-      response = response.replace("200", "500");
-    }
-  }
-
-}
-**/
